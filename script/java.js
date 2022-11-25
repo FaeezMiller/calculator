77c6224374293d8726f5583c0ba77eaebdafe5ee
Faeez Miller <faeezmiller8@gmail.com>
2022-11-25 14:52:37 +0200

    update

--- script/java.js
+++ script/java.js
@@ -1,18 +1,49 @@
+ //This function displays values
+let output = document.getElementById("display-box");
+let allowComa = true ;
 
-function clearScreen() {
-  document.getElementById("result").value = "";
+function display(anyVal) {
+  output.value = output.value + anyVal;
 }
- 
+ // This function evaluates(eval) the expression and return result
+  function myoperations(anyVal){
+    output.value = output.value + anyVal;
+    allowComa = true;
+  }
+  
+  //This function allows the "." to not return
+  function mydot(anyVal){
+    if (allowComa===true){
+      output.value = output.value + anyVal;
+      allowComa = false;
+    }
+  }
+  
+  // This function clear all the values
+  function clearScreen() {
+    output.value = "";
+    output.value = output.value + anyVal;
+    allowComa = true;
+  }
 
-function display(value) {
-  document.getElementById("result").value += value;
+
+  function calculate() {
+  try{
+    output.value = eval(output.value)
+  }
+  catch(err) {
+    output.value = "wrong"
+  }
 }
 
 
-function calculate() {
-  var p = document.getElementById("result").value;
-  var q = eval(p);
-  document.getElementById("result").value = q;
-}
+
+
+
+
+
+
+  
+  
 
 
